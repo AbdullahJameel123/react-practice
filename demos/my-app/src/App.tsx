82ffe0e35a429efdf85d6560f3fbddb7cd428c53
AbdullahJameel123 <askerijameelaskeri@gmail.com>
2025-08-04 04:20:44 +0530

    Day 4 demos practiced

--- demos/my-app/src/App.tsx
+++ demos/my-app/src/App.tsx
@@ -1,20 +1,20 @@
-<<<<<<< HEAD
 import EmployeeTable from "./Components/EmployeeTable";
-=======
+import Greeting from "./Components/Greeting";
+import ProfileCard from "./Components/ProfileCard";
 import Courses from "./Courses";
->>>>>>> b5cba0dc2f0a4a1129e19a9d1d0e83dde6254a9c
-
 
 function App() {
   return (
     <div>
       <h1>Welcome to My App</h1>
-<<<<<<< HEAD
+
       <p>This is a simple React application.</p>
-      <EmployeeTable />
-=======
-      <Courses></Courses>
->>>>>>> b5cba0dc2f0a4a1129e19a9d1d0e83dde6254a9c
+      <Greeting name="Mohammed Iliyas" />
+      <ProfileCard
+        name="Abdullah Askeri"
+        role="Software Engineer"
+        skills={["JavaScript", "React", "Node.js"]}
+      />  
     </div>
   );
 }
